0d9d7f528d647a7e7283fa4f532c58af1f0b2348
Mazen Ali <mazen.ali@multiversecomputing.com>
2025-06-23 17:36:56 +0200

    add tools and api keys

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from smolagents import CodeAgent, DuckDuckGoSearchTool, HfApiModel, load_tool, tool, LiteLLMModel
+from smolagents import CodeAgent, DuckDuckGoSearchTool, load_tool, tool, LiteLLMModel
+import os
 import datetime
 import requests
 import pytz
@@ -9,14 +10,15 @@ from Gradio_UI import GradioUI
 
 # Below is an example of a tool that does nothing. Amaze us with your creativity !
 @tool
-def my_custom_tool(arg1:str, arg2:int)-> str: #it's import to specify the return type
-    #Keep this format for the description / args / args description but feel free to modify the tool
-    """A tool that does nothing yet 
+def search_web(query: str, num_results: int) -> str:
+    """Searches the web for a query and returns the top results.
     Args:
-        arg1: the first argument
-        arg2: the second argument
+        query: The search query.
+        num_results: Number of results to return.
     """
-    return "What magic will you build ?"
+    search_tool = DuckDuckGoSearchTool(max_results=num_results)
+    results = search_tool(query)
+    return f"Top {num_results} results for '{query}':\n" + "\n".join(results)
 
 @tool
 def get_current_time_in_timezone(timezone: str) -> str:
@@ -47,21 +49,22 @@ model = LiteLLMModel(
 
 
 # Import tool from Hub
-image_generation_tool = load_tool("agents-course/text-to-image", trust_remote_code=True)
+image_generation_tool = load_tool("agents-course/text-to-image", token=os.getenv("HF_TOKEN"), trust_remote_code=True)
 
 with open("prompts.yaml", 'r') as stream:
     prompt_templates = yaml.safe_load(stream)
     
 agent = CodeAgent(
     model=model,
-    tools=[final_answer], ## add your tools here (don't remove final answer)
+    tools=[final_answer, search_web, image_generation_tool], ## add your tools here (don't remove final answer)
     max_steps=6,
     verbosity_level=1,
     grammar=None,
     planning_interval=None,
     name=None,
     description=None,
-    prompt_templates=prompt_templates
+    prompt_templates=prompt_templates,
+    additional_authorized_imports=["matplotlib.pyplot", "IPython.display"]
 )
 
 
